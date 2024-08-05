f3d3bbb9fb161cdafc22408525e8a67fae4009a2
joryeoha <yeoncheong08@gmail.com>
2024-08-05 22:26:35 +0900

    base

---
+++ src/main/java/ingang/ingang/IngangApplication.java
@@ -0,0 +1,13 @@
+package ingang.ingang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ingang/ingang/IngangApplicationTests.java
@@ -0,0 +1,13 @@
+package ingang.ingang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
