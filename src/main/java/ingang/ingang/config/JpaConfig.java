994484769f56f6a4a1a9a153dc6f999f6b5a1148
joryeoha <yeoncheong08@gmail.com>
2024-08-12 23:17:04 +0900

    feat : Swagger, Jpa config 설정

---
+++ src/main/java/ingang/ingang/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package ingang.ingang.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = {"ingang.ingang"})
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
