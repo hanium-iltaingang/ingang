a855f38f83999faf8ab2f3eb1d97bb3c4e8d9e56
joryeoha <yeoncheong08@gmail.com>
2024-08-12 23:15:22 +0900

    feat : 노트 기능 개발 (1)
    - NoteEntity 생성

---
+++ src/main/java/ingang/ingang/domain/TimeStamp.java
@@ -0,0 +1,24 @@
+package ingang.ingang.domain;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EnableJpaAuditing
+@EntityListeners(AuditingEntityListener.class)
+public class TimeStamp {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private  LocalDateTime modifiedAt;
+}
