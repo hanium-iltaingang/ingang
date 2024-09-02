3d9f532eaa3f34efe9ed95af08a53b65a4f0142e
JieunHan2001 <gks3177@naver.com>
2024-09-02 15:31:06 +0900

    Delete all files

--- ingang_app/src/App.test.js
+++
@@ -1,9 +0,0 @@
-// React 애플리케이션을 테스트하기 위한 코드
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- ingang_app/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // 'react-dom/client'에서 createRoot를 가져옵니다.
-import './App.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
