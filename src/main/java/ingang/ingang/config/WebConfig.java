f576014494e8040fb3162ec414a1480b93b41168
joryeoha <yeoncheong08@gmail.com>
2024-09-17 17:22:22 +0900

    chore: application.properties, WebConfig 파일 추가

---
+++ src/main/java/ingang/ingang/config/WebConfig.java
@@ -0,0 +1,18 @@
+package ingang.ingang.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 모든 경로에 대해 CORS 설정을 적용
+                .allowedOrigins("http://localhost:3000")  // 리액트 앱의 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메서드
+                .allowedHeaders("*")  // 모든 헤더 허용
+                .allowCredentials(true);  // 인증 정보(Cookies 등) 허용
+    }
+}
